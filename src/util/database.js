9e03ae3bef59767c78c49d5feadac9f947805b71
marufbelete9@gmail.com <marufbelete9@gmail.com>
2023-08-22 15:10:01 +0300

    deploy

--- src/util/database.js
+++ src/util/database.js
@@ -2,12 +2,12 @@ const {Sequelize}=require('sequelize');
 const config = require('../config/config');
 const sequelize = new Sequelize(config.DATABASE_URL, {
   logging: false,
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false,
-  //   },
-  // },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
  
