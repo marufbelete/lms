b10461e588a73ea15842898163664327a6fc2cbd
Jonathon <jonathon@pixel8.io>
2023-09-25 17:23:52 -0400

    add type to exercise

---
+++ migrations/20230925212129-add-type-to-exercise.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("exercises", "type", {
+      type: Sequelize.STRING,
+      allowNull: true,
+      defaultValue: null,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn("exercises", "type");
+  },
+};
--- src/models/exercise.model.js
+++ src/models/exercise.model.js
@@ -26,6 +26,10 @@ const Exercise = sequelize.define("exercise", {
     type: Sequelize.INTEGER,
     allowNull: true,
   },
+  type: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
 });
 
 module.exports = Exercise;
